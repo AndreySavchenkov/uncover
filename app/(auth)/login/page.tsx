5d5b605136e762cd26c75ab80c4c3331f850ae80
AndreySavchenkov <sannndvik16@gmail.com>
2025-10-24 20:23:58 +0200

    feat: login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,54 +1,132 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { supabase } from '@/lib/supabaseClient'
-import { useRouter } from 'next/navigation'
+import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Controller, useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+import { Button } from "@/components/ui/button";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  email: z.email("Must be an email"),
+  password: z.string(),
+});
 
 export default function LoginPage() {
-  const router = useRouter()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState<string | null>(null)
+  const router = useRouter();
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-    if (error) {
-      setError(error.message)
-    } else {
-      router.push('/profile')
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(formData: z.infer<typeof formSchema>) {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: formData.email,
+        password: formData.password,
+      });
+
+      if (error) {
+        toast.error(`${error.message}`, {
+          position: "top-center",
+        });
+      }
+
+      if (!error) {
+        toast.success("Success!", {
+          position: "top-center",
+        });
+        router.push("/");
+      }
+    } catch (error) {
+      toast.error(`Error: ${error}`, {
+        position: "top-center",
+      });
     }
   }
 
   return (
     <div className="max-w-md mx-auto mt-20">
-      <h1 className="text-2xl font-bold mb-4">Log In</h1>
-      <form onSubmit={handleLogin} className="flex flex-col gap-3">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-        >
-          Log In
-        </button>
-        {error && <p className="text-red-500">{error}</p>}
-      </form>
+      <Card className="w-full sm:max-w-md">
+        <CardHeader>
+          <CardTitle>Login Form</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form id="form" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="form-email">Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id="form-email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter your email"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="form-password">Password</FieldLabel>
+                    <PasswordInput
+                      {...field}
+                      id="form-password"
+                      placeholder="Enter your password"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Field orientation="horizontal">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => form.reset()}
+            >
+              Reset
+            </Button>
+            <Button type="submit" form="form">
+              Login
+            </Button>
+          </Field>
+        </CardFooter>
+      </Card>
     </div>
-  )
+  );
 }
